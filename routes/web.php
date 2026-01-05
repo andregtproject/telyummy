99044e3e8c0b04e71783442702cf4912889d51aa
nurillai <nurilababielirawan01@gmail.com>
2026-01-05 16:16:19 +0700

    route tenant

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::resource('canteens', CanteenController::class);
+});
+
 require __DIR__.'/auth.php';
